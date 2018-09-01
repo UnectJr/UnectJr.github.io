bc572300b470877d423d75dcef049b5370d1c623
Guilherme Miua <guilhermemiua@gmail.com>
2018-09-01 15:05:36 -0300

    Modal bug fixed

--- js/init.js
+++ js/init.js
@@ -36,16 +36,23 @@ window.onload = function(){
 /* MODAL */
 // Get the modal
 var modal = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+btn2.onclick = function () {
+    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -53,36 +60,20 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-/* MODAL 2 */
-// Get the modal
-var modal2 = document.getElementById('myModal2');
-
-// Get the button that opens the modal
-var btn2 = document.getElementById("myBtn2");
-
-// Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close2")[0];
-
-// When the user clicks on the button, open the modal 
-btn2.onclick = function () {
-    modal2.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
 span2.onclick = function () {
     modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+
     if (event.target == modal2) {
         modal2.style.display = "none";
     }
 }
+
+
+
